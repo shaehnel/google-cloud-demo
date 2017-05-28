9cabde0750a515dcabb943ae01fec6ecd357a37f
Sebastian Haehnel <sebastian.haehnel@gmail.com>
2017-05-28 09:22:40 +0200

    first commit

---
+++ src/main/java/com/trashgroup/googleclouddemo/GoogleCloudDemoApplication.java
@@ -0,0 +1,12 @@
+package com.trashgroup.googleclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudDemoApplication.class, args);
+	}
+}
